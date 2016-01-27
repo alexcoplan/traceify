6cece5d823495923a8120ba0864e429374364036
Alex Coplan <lexy0202@gmail.com>
2016-01-27 11:02:06 +0000

    setup pages

--- src/light.cpp
+++
@@ -1,8 +0,0 @@
-#include "light.hpp"
-
-Light::Light(const vec3& position, RGBVec col)
-	: pos(position), colour(col) {}
-
-vec3 Light::lVectorFromPoint(const vec3 &p) {
-	return (pos - p).normalised();
-};
